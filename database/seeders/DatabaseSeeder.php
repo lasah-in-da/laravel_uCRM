df5438198ca4e19dce274cbf91817b23ea4c225b
lasah <fumiki.ihara@gmail.com>
2023-09-10 14:57:41 +0900

    customers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         ]);
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Customer::factory(1000)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
---
+++ resources/js/Pages/Customers/Index_test.vue
@@ -0,0 +1,19 @@
+<script setup>
+import FlashMessage from '@/Components/FlashMessage.vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    customers: Object
+})
+
+</script>
+
+<template>
+    <ul v-for="customer in customers.data" :key="customer.id">
+        <li>{{ customer.id }}</li>
+        <li>{{ customer.name }}</li>
+    </ul>
+    <Pagination class="mt-6" :links="customers.links"></Pagination>
+</template>
